ac70bc3691ec3aa17915f61d40d061c55ec0ade8
Marco Mangan <marco.mangan@gmail.com>
2018-05-02 15:50:34 -0300

    Criacao do projeto no Eclipse

---
+++ exemplo02/src/main/java/pucrs/s2b/exemplo02/App.java
@@ -0,0 +1,13 @@
+package pucrs.s2b.exemplo02;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
