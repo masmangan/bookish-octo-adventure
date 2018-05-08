5c94a156afc0d18c6293bea8551e866ccd22bdfe
Marco Mangan <marco.mangan@gmail.com>
2018-05-08 14:40:54 -0300

    #2

--- exemplo02/src/test/java/pucrs/s2b/exemplo02/HelloTest.java
+++ exemplo02/src/test/java/pucrs/s2b/exemplo02/HelloTest.java
@@ -1,21 +1,31 @@
 package pucrs.s2b.exemplo02;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+
+@RunWith(JUnitParamsRunner.class)
 public class HelloTest {
 
-	@Test
-	public void testSomar() {
-		int expected = 50;
-		int actual = Hello.somar(20, 30);
+	@Test	
+	@Parameters({"20, 30, 50", 
+                  "0, 0, 0" })
+	public void testSomarP(int a, int b, int r) {
+		int expected = r;
+		int actual = Hello.somar(a, b);
 		assertEquals(expected, actual);	
 	}
-
-	@Test
-	public void testSomar2() {
-		assertEquals(0, Hello.somar(0, 0));
-	}	
 	
 }
+
+
+
+
+
+
+
+
